55d11e929ee7a0040f0c95ffa9bc9f291369804e
zhaojs-a <zhaojs-a@glodon.com>
2021-10-18 17:29:45 +0800

    init

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String x = "Hello, World!";
+        System.out.println(x);
+    }
+}
---
+++ src/Puppy.java
@@ -0,0 +1,19 @@
+public class Puppy{
+    int puppyAge;
+    public Puppy(String name){
+        System.out.println(name);
+    }
+    public void setAge(int age){
+        puppyAge=age;
+    }
+    public int getAge(){
+        System.out.println(puppyAge);
+        return puppyAge;
+    }
+    public static void main(String[] args) {
+        Puppy myPuppy=new Puppy("zhangsan");
+        myPuppy.setAge(2);
+        myPuppy.getAge();
+        System.out.println(myPuppy.puppyAge);
+    }
+}
